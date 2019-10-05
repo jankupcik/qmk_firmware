a13e6d071d018ff616101bf88bc48ed8f4e721ad
H <H>
2019-10-05 14:44:40 +0200

    Change bracket layout to optimize Java development: [1, L1]: from < [ ( { to [ { ( <

--- keyboards/redox/keymaps/jk/keymap.c
+++ keyboards/redox/keymaps/jk/keymap.c
@@ -48,8 +48,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //│         │   F1    │   F2    │   F3    │   F4    │   F5    │                                                      │   F6    │   F7    │   F8    │   F9    │   F10   │   F11   │
      XXXXXXX  ,KC_F1    ,KC_F2    ,KC_F3    ,KC_F4    ,KC_F5    ,                                                       KC_F6    ,KC_F7    ,KC_F8    ,KC_F9    ,KC_F10   ,KC_F11   ,
   //├─────────┼─────────┼─────────┼─────────┼─────────┼─────────┼─────────┐                                  ┌─────────┼─────────┼─────────┼─────────┼─────────┼─────────┼─────────┤
-  //│         │    <    │    [    │    (    │    {    │         │         │                                  │         │         │    }    │    )    │    ]    │    >    │   F12   │
-     KC_TAB   ,KC_LT    ,KC_LBRC  ,KC_LPRN  ,KC_LCBR  ,XXXXXXX  ,XXXXXXX  ,                                   XXXXXXX  ,KC_SLSH  ,KC_RCBR  ,KC_RPRN  ,KC_RBRC  ,KC_GT    ,KC_F12   ,
+  //│         │    [    │    {    │    (    │    <    │         │         │                                  │         │    /    │    >    │    )    │    }    │    ]    │   F12   │
+     KC_TAB   ,KC_LBRC  ,KC_LCBR  ,KC_LPRN  ,KC_LT    ,XXXXXXX  ,XXXXXXX  ,                                   XXXXXXX  ,KC_SLSH  ,KC_GT    ,KC_RPRN  ,KC_RCBR  ,KC_RBRC  ,KC_F12   ,
   //├─────────┼─────────┼─────────┼─────────┼─────────┼─────────┼─────────┤                                  ├─────────┼─────────┼─────────┼─────────┼─────────┼─────────┼─────────┤
   //│         │    1    │    2    │    3    │    4    │    5    │         │                                  │         │    6    │    7    │    8    │    9    │    0    │    '    │
      _______  ,KC_1     ,KC_2     ,KC_3     ,KC_4     ,KC_5     ,XXXXXXX  ,                                   XXXXXXX  ,KC_6     ,KC_7     ,KC_8     ,KC_9     ,KC_0     ,KC_QUOT  ,
